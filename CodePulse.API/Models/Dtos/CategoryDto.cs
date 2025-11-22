7ed9a462d4cdaf1d8261ccbf05c23b1912ad1d90
guicesars <guicesarsosil@gmail.com>
2025-11-22 17:15:04 -0300

    feat: add API and Angular UI into monorepo structure

--- CodePulse.API/Models/Dtos/CategoryDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CodePulse.API.Models.Dtos;
-
-public class CategoryDto
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public string UrlHandle { get; set; }
-}
--- CodePulse.API/Repositories/Interfaces/ICategoryRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using CodePulse.API.Models.Domain;
-
-namespace CodePulse.API.Repositories.Interfaces;
-
-public interface ICategoryRepository
-{
-    Task<Category> CreateAsync(Category category);
-}
