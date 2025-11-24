f96e40b84b43ff6327078e7650670cc806f80d41
guicesars <guicesarsosil@gmail.com>
2025-11-24 12:42:42 -0300

    feat: create categories list component and angular routing

--- src/CodePulseUI/CodePulse/src/app/app.routes.ts
+++ src/CodePulseUI/CodePulse/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CategoryList } from './features/category/category-list/category-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'admin/categories',    // URL path for categories page
+        component: CategoryList      // Component to display when visiting the path
+    }
+];
--- src/CodePulseUI/CodePulse/src/app/core/components/navbar/navbar.ts
+++ src/CodePulseUI/CodePulse/src/app/core/components/navbar/navbar.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink], // Import RouterLink for navigation
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
