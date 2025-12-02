17b5766d40416e3b2ecb593ade666739c82eb72a
guicesars <guicesarsosil@gmail.com>
2025-12-02 14:22:00 -0300

    feat(UI): get all blogposts and display them

--- src/CodePulseUI/CodePulse/src/app/features/Blogpost/services/blog-post-service.ts
+++ src/CodePulseUI/CodePulse/src/app/features/Blogpost/services/blog-post-service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { AddBlogPostRequest, BlogPost } from '../models/blogpost.model';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, httpResource, HttpResourceRef } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
 import { Observable } from 'rxjs';
 
@@ -15,5 +15,10 @@ export class BlogPostService {
   createBlogPost(data: AddBlogPostRequest): Observable<BlogPost>
   {
     return this.http.post<BlogPost>(`${this.apiBaseUrl}/api/BlogPost`, data)
+  }
+
+  getAllBlogPosts(): HttpResourceRef<BlogPost[] | undefined>
+  {
+    return httpResource<BlogPost[]>( ()=> `${this.apiBaseUrl}/api/BlogPost`)
   }
 }
