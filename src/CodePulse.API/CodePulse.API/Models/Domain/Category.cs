3e8993bbaeb6a5fd2b57637d8d344e9b68b86791
guicesars <guicesarsosil@gmail.com>
2025-12-02 17:28:27 -0300

    feat(api): change domain models and add ef core relationships

--- src/CodePulse.API/CodePulse.API/Models/Domain/Category.cs
+++ src/CodePulse.API/CodePulse.API/Models/Domain/Category.cs
@@ -5,4 +5,5 @@ public class Category
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Name { get; set; } = string.Empty;
     public string UrlHandle { get; set; } = string.Empty;
+    public ICollection<BlogPost>? Blogposts { get; set; }
 }
