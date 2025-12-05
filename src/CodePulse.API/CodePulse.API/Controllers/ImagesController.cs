322cdb6550b4b62ad4622f1276ca032bb4773874
guicesars <guicesarsosil@gmail.com>
2025-12-05 17:11:00 -0300

    feat(api): upload image controller and action method

---
+++ src/CodePulse.API/CodePulse.API/Controllers/ImagesController.cs
@@ -0,0 +1,42 @@
+﻿using CodePulse.API.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ImagesController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> UploadImage(
+        [FromForm] IFormFile file, 
+        [FromForm] string fileName,
+        [FromForm] string title)
+    {
+        ValidateForm(file);
+
+        if(ModelState.IsValid)
+        {
+            var blogImage = new BlogImage
+            {
+                FileExtension = Path.GetExtension(file.FileName).ToLower(),
+                FileName = fileName,
+                Title = title,
+                DateCreated = DateTime.UtcNow
+            };
+        }
+
+        return Ok();
+    }
+    private void ValidateForm(IFormFile file)
+    {
+        var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png" };
+
+        if(!allowedExtensions.Contains(Path.GetExtension(file.FileName).ToLower()))
+            ModelState.AddModelError("file", "Invalid file type. Only .jpg, .jpeg, and .png are allowed.");
+
+        if (file.Length > 10485760)
+            ModelState.AddModelError("file", "File size cannot be more than 10MB");
+        
+    }
+}
