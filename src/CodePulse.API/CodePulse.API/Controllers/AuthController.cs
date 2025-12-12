421210c16151bef6cb7e7b204f4179c385673fb1
guicesars <guicesarsosil@gmail.com>
2025-12-12 14:06:48 -0300

    feat(auth): create ayth controller and register action method

---
+++ src/CodePulse.API/CodePulse.API/Controllers/AuthController.cs
@@ -0,0 +1,60 @@
+﻿using CodePulse.API.Models.Dtos.Auth;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public AuthController(UserManager<IdentityUser> userManager)
+    {
+        _userManager = userManager;
+    }
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody]AuthRequestDto authRequest)
+    {
+        var user = new IdentityUser
+        {
+            UserName = authRequest.Email?.Trim(),
+            Email = authRequest.Email?.Trim(),
+        };
+
+        var identityResult = await _userManager.CreateAsync(user, authRequest.Password);
+
+        if(identityResult.Succeeded)
+        {
+            
+            await _userManager.AddToRoleAsync(user, "Reader");
+
+            if (identityResult.Succeeded)
+                return Ok();
+            else
+            {
+                if (identityResult.Errors.Any())
+                {
+                    foreach (var error in identityResult.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+
+        }
+        else
+        {
+            if(identityResult.Errors.Any())
+            {
+                foreach(var error in identityResult.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+        }
+
+        return ValidationProblem(ModelState);
+    }
+}
--- src/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
+++ src/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
@@ -2,6 +2,7 @@
 using CodePulse.API.Models.Domain;
 using CodePulse.API.Models.Dtos.Category;
 using CodePulse.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodePulse.API.Controllers;
